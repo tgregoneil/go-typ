d195cacc1b42ed54b64cd184ad1a148aca1c6d7d
T. Greg O'Neil <tgregoneil@gmail.com>
2023-08-05 16:59:17 -0500

    next

--- test.js
+++ test.js
@@ -1,2 +1,21 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 // test.js
+
+const gt = require('go-typ');
+const items = {
+    o: {a:1, b:2},
+    s: 'stringy',
+    i: 55,
+    r: 55.5,
+    b: true,
+    a: [6,7],
+};
+
+const ia = Object.keys(items);
+
+ia.forEach(function(item) {
+
+    const res = gt.res(items[item]);
+    console.log(`${item}: ${res}`);
+});
+
